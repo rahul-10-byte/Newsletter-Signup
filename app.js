405de0dd9e4b862ff4d66a34e2d7b4a7b7321a98
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-12 10:16:49 +0530

    created Routes

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email);
+});
 
 
 app.listen(3000, function () {
