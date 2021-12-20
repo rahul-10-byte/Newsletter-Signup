f7921d5a9625ba6716057482808fca8903bc985d
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-20 09:07:51 +0530

    Setup Failure and Success Pages

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ app.post("/", function (req, res) {
     };
 
     const request = https.request(url, options, function (response) {
+
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         });
@@ -60,9 +67,6 @@ app.listen(3000, function () {
     console.log("Server started at 3000!!");
 });
 
-
-//API key
-// 428a1a0fbfe4c3bdf06016f4f4db4d34-us20
 
 // list id
 // 4ca08aef41
