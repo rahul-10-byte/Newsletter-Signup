1e4f766706c2ceb1b31f06ef027bd334634fa2fd
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-23 15:54:55 +0530

    Deploying to Heroku

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started at 3000!!");
 });
 
